f80b663b82a0cf4d71095b69d024f690ec44b795
Yago Santos de Sousa <yagosansz@gmail.com>
2019-10-31 00:39:04 -0400

    Implemented call to api

--- reactjs-huntweb/src/App.js
+++ reactjs-huntweb/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import './styles.css'
+import './styles.css';
 
 import Header from './components/Header';
+import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 

07d40df900117b6c7f41247107b23f5614120287
Yago Santos de Sousa <yagosansz@gmail.com>
2019-11-01 00:34:17 -0400

    Storing variables in a state variable

--- reactjs-huntweb/src/pages/main/index.js
+++ reactjs-huntweb/src/pages/main/index.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        products: [],
+    }
+
+    // Executed when the componenet is displayed
     componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get('/products');
-        console.log(response.data.docs);
-    };
+        
+        this.setState({ products: response.data.docs });
+    }; 
 
+    // Listen for changes in the state variable...
     render() {
-        return <h1>Hello Rocketseat</h1>
+        return (
+            <div className="product-list">
+                { this.state.products.map(product => (
+                    <h2 key={ product._id }>{ product.title }</h2>
+                )) }
+            </div>
+        )
     }
 }

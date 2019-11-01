f1f58b15c7dcfb9246dbb56ca8afdd79dd51274f
Yago Santos de Sousa <yagosansz@gmail.com>
2019-11-01 11:46:00 -0400

    Implemented pagination

--- reactjs-huntweb/src/pages/main/index.js
+++ reactjs-huntweb/src/pages/main/index.js
@@ -6,6 +6,8 @@ import './styles.css';
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     // Executed when the componenet is displayed
@@ -13,16 +15,40 @@ export default class Main extends Component {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
-        
-        this.setState({ products: response.data.docs });
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
     }; 
+
+    prevPage = () => {
+        const { page } = this.state;
+
+        // Do nothing if already on first page
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        // Do nothing if already on last page
+        if(page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
 
     // Listen for changes in the state variable...
     render() {
 
-        const { products } = this.state;
+        const { products, productInfo, page } = this.state;
 
         return (
             <div className="product-list">
@@ -34,6 +60,10 @@ export default class Main extends Component {
                         <a href="#">Access</a>
                     </article>
                 )) }
+                <div className="actions">
+                    <button disabled={ page === 1 } onClick={ this.prevPage }>Previous</button>
+                    <button disabled={ page === productInfo.pages } onClick={ this.nextPage }>Next</button>
+                </div>
             </div>
         )
     }
